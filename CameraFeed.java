f607c640aef1540861a96bc534efad282aa7e5db
Damien <93683037+KillerCube1@users.noreply.github.com>
2024-04-05 09:37:08 -0400

    Add files via upload

---
+++ CameraFeed.java
@@ -0,0 +1,30 @@
+import com.github.sarxos.webcam.Webcam;
+
+import java.awt.image.BufferedImage;
+
+public class CameraFeed extends Thread {
+
+    private boolean running;
+
+    @Override
+    public void run() {
+
+        running = true;
+
+        while (running) {
+
+            BufferedImage frame = Camera.getCam().getImage(); // Gets image
+
+            if (frame != null) {
+                OpenCVData.getYellow(frame);
+            }
+
+        }
+
+    }
+
+    public void stopThread() {
+        running = false;
+    }
+
+}
